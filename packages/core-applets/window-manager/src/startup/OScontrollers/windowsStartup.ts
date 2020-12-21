a9aedf87745ae99d38fdd805f41eee7e4d866370
TarVK <tarvankrieken@gmail.com>
2020-12-21 23:38:15 +0100

    Replace copy startup install with shortcut

--- packages/core-applets/window-manager/src/startup/OScontrollers/windowsStartup.ts
+++ packages/core-applets/window-manager/src/startup/OScontrollers/windowsStartup.ts
@@ -2,16 +2,25 @@ import {IStartupController} from "../_types/IStartupController";
 import Path from "path";
 import FS from "fs";
 import {promisify} from "util";
+import ws from "windows-shortcuts";
 
 const startupFolder =
     "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp";
 function getTargetPath(inputLocation: string): string {
     const fileName = Path.basename(inputLocation);
-    return Path.join(startupFolder, fileName);
+    const shortcutName = fileName.replace(/\.[^.]*$/, ".lnk");
+    return Path.join(startupFolder, shortcutName);
 }
 
 export const windowStartup: IStartupController = {
-    register: async location => promisify(FS.copyFile)(location, getTargetPath(location)),
+    register: async location => {
+        return new Promise((res, rej) => {
+            ws.create(getTargetPath(location), location, error => {
+                if (error) rej(error);
+                else res();
+            });
+        });
+    },
     deregister: async location => promisify(FS.unlink)(getTargetPath(location)),
     isRegistered: async location => FS.existsSync(getTargetPath(location)),
 };
