978782578bcfe0dbfc48d2760a761ded4d967330
TarVK <tarvankrieken@gmail.com>
2021-03-07 01:49:41 +0100

    Fixed small setup mistakes

--- packages/core/src/menus/items/inputs/handlers/color/ColorInput.tsx
+++ packages/core/src/menus/items/inputs/handlers/color/ColorInput.tsx
@@ -1,10 +1,11 @@
-import {IField, IInputConfig} from "@launchmenu/core";
 import Color from "color";
 import React from "react";
 import {ColorTextFieldView} from "../../../../../components/settings/inputs/ColorTextFieldView";
 import {ITextField} from "../../../../../textFields/_types/ITextField";
 import {Input} from "../../../../../uiLayers/types/input/Input";
 import {IViewStackItem} from "../../../../../uiLayers/_types/IViewStackItem";
+import {IInputConfig} from "../../../../../uiLayers/types/input/_types/IInputConfig";
+import {IField} from "../../../../../_types/IField";
 
 /**
  * An input layer for colors
@@ -23,16 +24,19 @@ export class ColorInput extends Input<string> {
      */
     public constructor(field: IField<string>, config: IInputConfig<string>);
     public constructor(field: IField<string>, config?: IInputConfig<string>) {
-        super(field, {checkValidity: text => {
-            try {
-                new Color(text);
-            } catch {
-                return {
-                    message: `'${text}' is not a valid color`,
-                    ranges: [{start: 0, end: text.length}],
-                };
-            }
-        }, ...config});
+        super(field, {
+            checkValidity: text => {
+                try {
+                    new Color(text);
+                } catch {
+                    return {
+                        message: `'${text}' is not a valid color`,
+                        ranges: [{start: 0, end: text.length}],
+                    };
+                }
+            },
+            ...config,
+        });
     }
 
     /** @override */
