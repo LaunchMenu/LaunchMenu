c171cc56753badc87e000a987559616c0bd3ffb8
TarVK <tarvankrieken@gmail.com>
2021-06-14 17:32:34 +0200

    removed some UI flashing on window showing

--- packages/core-applets/window-manager/src/visibility/OScontrollers/windowsVisibility.ts
+++ packages/core-applets/window-manager/src/visibility/OScontrollers/windowsVisibility.ts
@@ -5,27 +5,28 @@ const windowsVisibilityController: IVisibilityController = {
     init(window) {},
     show(window) {
         if (window.isMinimized()) {
-            // https://stackoverflow.com/a/60314425/8521718
             window.once("restore", () => {
-                setTimeout(() => {
-                    window.focus();
-                    window.moveTop();
-                }, 50);
+                window.focus();
+                window.moveTop();
             });
             window.restore();
         } else {
             // First time around, use the show method
             // https://stackoverflow.com/a/60314425/8521718
             window.once("show", () => {
-                setTimeout(() => {
-                    window.focus();
-                    window.moveTop();
-                }, 200);
+                window.focus();
+                window.moveTop();
             });
             window.show();
         }
+
+        // https://github.com/electron/electron/issues/12130#issuecomment-407306642
+        setTimeout(() => {
+            window.setOpacity(1);
+        }, 50);
     },
     hide(window) {
+        window.setOpacity(0);
         window.minimize();
     },
 };
